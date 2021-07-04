5b7dc3be78b4124f26d16fa2154135ab59f90084
share <ybzc.gg@gmail.com>
2021-07-04 08:48:23 +0800

    improve swiper pagination

--- plugins/vue-awesome-swiper.js
+++ plugins/vue-awesome-swiper.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import VueAwesomeSwiper from 'vue-awesome-swiper';
 
 // Import Swiper styles
-import 'swiper/swiper.scss';
+import 'swiper/css/swiper.css';
 //import "swiper/components/pagination/pagination.scss"
 
 // import custom style
